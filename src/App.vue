6a012a03c61e299390bf5dc4360337a876cddaed
andriferry <ferryandri35@gmail.com>
2022-12-25 09:09:19 +0700

    add footer

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,36 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import Header from "@/components/Header.vue";
+import Footer from "@/components/Footer.vue";
 </script>
 
 <template>
-  <Header />
+<div class="main">
+   <Header />
 
-  <main class="mt-5">
-      <RouterView />
+  <main class="mt-5 article">
+    <RouterView />
   </main>
 
+  <Footer class="footer" />
+</div>
 </template>
 
 <style scoped>
 body {
   font-family: 'Nunito Sans', sans-serif;
 }
+
+
+.main {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+
+.article {
+  flex: 1;
+}
+
+
 </style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,50 @@
+<script setup>
+
+const date = new Date()
+const openLink = () => {
+  window.open('https://tirebasics.com/')
+};  
+</script>
+
+
+<template>
+  <div class="border-top mt-auto w-100">
+    <div class="p-3 w-100 d-flex flex-column gap-3">
+      <p class="mb-0">
+        By using our website, you understand and agree that all information provided is "as is" and "as available". This means that we do not represent, warrant or guarantee either expressed or implied as to the accuracy of the product and fitment information provided on this site.
+        Use of this site implies your agreement to these terms.
+      </p>
+
+      <div class="w-100 d-flex flex-column flex-md-row gap-3 justify-content-between">
+        <div class="d-flex gap-1">
+          <p class="mb-0 anchor" @click="openLink">
+            Tirebasics.com
+          </p>
+          <p class="mb-0">
+             © {{date.getFullYear()}}.   All rights reserved.
+          </p>
+        </div>
+
+        <div class="d-flex flex-column flex-md-row gap-3">
+          <p class="mb-0">
+            Terms & Conditions 
+          </p>
+          <p class="mb-0">
+            Privacy Policy
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+p {
+  font-size: 10px;
+  font-weight: 500;
+  line-height: 13.64px;
+}
+.anchor {
+  cursor: pointer;
+}
+</style>
